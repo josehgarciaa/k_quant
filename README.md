5632ff57a8112ce4f1ab439f5479ce3af0477432
Jose H. Garcia <josehugo.garcia@protonmail.com>
2022-10-21 06:42:43 +0200

    Included an example in the readme page

--- README.md
+++ README.md
@@ -18,17 +18,54 @@ pip install .
 ```python
 
 from numpy import sqrt, exp, dot, conj
-from kquant import bands
+import kquant as k
 
-#Let us first consider the graphene Hamiltonian in momentum space
+"""
+Let us first define a Hamiltonian in momentum space
+and for this purpose we choose as prototype a model for p_z electrons
+ in graphene within the nearest neighbor approximation with lattice vectors
+ defined in the lat_vec variable
+"""
+lat_vec = [ [ 1/2, sqrt(3)/2 ], [ 1/2, sqrt(3)/2 ] ]
 def hamiltonian(k):
-    a_0 = [ 1/2, sqrt(3)/2 ];
-    a_1 = [ 1/2,-sqrt(3)/2 ];
+    a_0, a_1  = lat_vec;
     hop = 2.8;
-    f_k = hop*( 1 + exp( -1j*dot(k,a_0)) + exp( -1j*dot(k,a_0)) );
+    f_k = hop*( 1 + exp( -1j*dot(k,a_0)) + exp( -1j*dot(k,a_1)) );
     return [ [ 0        , f_k],
              [ conj(f_k),  0 ]
             ];
+
+#The band class requires lattice vectors and the hamiltonian function
+graphene = k.bands(lat_vec, hamiltonian );
+
+#To plot a desire band-path you pass it to the class as a list of tuples
+npts= 100;
+bandpath = [ ("K", (1/3,2/3,0), npts ), ("G", (0,0,0), npts) , ("M",1/2,1/2,0) ];
+graphene.set_bandpath( bandpath);
+
+#The computing the band structure is as simmple as
+bandstructure = graphene.compute_bands();
+for band in banstructure():
+    plt.plot(band);
+
+#In this particular scenario, the distance between the high-symmetry point in the band-structure
+#do not corresponds to the real distance within the Brilluoin Zone. This is why we provide 
+#function to get the proper xaxis in the bandstructure plot
+xaxis =  graphene.Xaxis();
+for band in banstructure():
+    plt.plot(xaxis,band);
+
+#In several situations, it is desireable to compute the projection of orbital or spins in these bands. 
+#For instance, in graphene, one would like to compute the so called pseudo-spin projected bandstructures. 
+#For that case we need to define the projection operator
+sigma_z = [ [1, 0],
+            [0,-1]]
+
+# then we can proceed with the calculation of the projection
+#The computing the band structure is as simmple as
+projections = graphene.compute_bands( projection_operators = sigma_z);
+for band in banstructure():
+    plt.plot(band);
 
 
 ```
--- k-quant/bands.py
+++ k-quant/bands.py
@@ -26,11 +26,11 @@ class bands:
 
     """
        
-    def __init__(self, label ):
+    def __init__(self, lat_vec, H_k ):
               
-        self.lat_vec   = None;
+        self.lat_vec   = lat_vec;
         self.bandpath  = np.array( ['G',1,[0,0,0]] );
-        self.H_k       = None;
+        self.H_k       = H_k;
 
     def set_hamiltonian_k(self, H_k):
         """_summary_
@@ -104,7 +104,7 @@ class bands:
 
 
     def XLabels(self ):
-        """Get the x-axis labels associated with the bandpath
+        """Returns the x-axis labels associated with the bandpath
 
         Returns:
             list: _description_
@@ -112,17 +112,34 @@ class bands:
         Xaxis = self.bandsXaxis()
         xpos = (np.cumsum(self.bandpath[:,1],dtype=int)-1)
         return (Xaxis[xpos],self.bandpath[:,0])                
-    
-    
-    def bandsXaxis(self ): #The kpoints are assume to be in absolute coordinates 
+       
+    def Xaxis(self ):
+        """Returns a unique x-axis parameter that respect the distance between k-points
+
+        Returns:
+            ndarray: An array of k-point
+        """
+        
         kpoints=self.band_kpoints(absolute_coords = True);
         Xaxis=np.cumsum(np.linalg.norm(np.diff( kpoints,axis=0, prepend = 0),axis=1));
         Xaxis-=Xaxis[0];#Remove the initial value.
         return Xaxis;
     
     
-    def compute_band_structure(self, fermi_energy = 0.0, proj_op = None, ax=None, plot_proj=False, proj_range=None ):
-     
+    def compute_band_structure(self, fermi_energy = 0.0, proj_op = None ):
+        """_summary_
+
+        Args:
+            fermi_energy (float, optional): _description_. Defaults to 0.0.
+            proj_op (_type_, optional): _description_. Defaults to None.
+            ax (_type_, optional): _description_. Defaults to None.
+            plot_proj (bool, optional): _description_. Defaults to False.
+            proj_range (_type_, optional): _description_. Defaults to None.
+
+        Returns:
+            _type_: _description_
+        """
+
         #Compute the kpoints based on the path
         kpoints= self.band_kpoints();
 
@@ -137,211 +154,7 @@ class bands:
         projs = peigenvals[:,1].T;
         
         return self.plot_band_structure( bands = bands , projs = projs , ax=ax, plot_proj = plot_proj, proj_range=proj_range);
-
-    
-    def plot_band_structure(self, bands, projs = None, ax=None, plot_proj = False, proj_range=None):
-        
-        xaxis  = self.bandsXaxis();
-            
-        #PLOTING
-        #plot options
-        if  ax is None:
-            fig = plt.figure();
-            _ax = fig.add_subplot();
-        else:
-            _ax  = ax;
-
-        _ax.tick_params(axis='both', which='major', labelsize=16);
-        
-        if plot_proj is True:
-            for i,proj in enumerate(projs):
-                _ax.plot(xaxis,proj );
-        else:
-            _ax.set_ylabel("Energy (eV)", fontsize=16);
-            vmin,vmax = 0,1;
-            if projs is not None:
-                vmin = np.min(projs);
-                vmax = np.max(projs);
-                if proj_range is not None:
-                    vmin,vmax = list(proj_range);
-            for i,band in enumerate(bands):
-                _ax.plot(xaxis,band , color = 'k', lw=band_lw);
-                c = 'k';
-                if projs is not None:
-                    c = projs[i];
-                #Add points to the plot
-                im = _ax.scatter(xaxis, band,s=proj_s, c=c,cmap=cmap_name,vmin=vmin, vmax=vmax);
-                im.set_facecolor("none");        
-            #Create falso plot for color bar
-            if projs is not None and ax is None: #Add the color bar whenever you have the a projectedprlt
-                im = _ax.scatter(xaxis, bands[0],s=0, c=np.linspace( vmin,vmax,len(bands[0]) ),cmap=cmap_name);
-                fig.colorbar(im, ax=_ax); 
-
-        klabels, path_labels = self.get_XLabels()
-        _ax.set_xticks(klabels);
-        _ax.set_xticklabels(path_labels);
-                
-        if ax is None:
-
-            return fig,_ax;
-
-        ax = _ax;
-        return ax;
-
-    def compute_dispersion(self,kpoints, proj_op=None, absolute_coords = False):
-        if ( absolute_coords is True ):
-            Abs2Rec = np.linalg.inv(np.transpose(self.Momentum_Rec2AbsMatrix() ) );
-            kpoints = np.dot( kpoints , Abs2Rec);
-        
-        return np.array( [self.projected_eigenvalues( kp, proj_op ) for kp in kpoints ] );        
-      
-    def compute_fermi_surface(self,kpoints, fermi_energy = 0.0, tol = None ,  proj_op = None ):
-
-        #Compute the eigenvalues and the projected values 
-        peigenvals  = self.compute_dispersion(kpoints, proj_op=proj_op)
-        eigenvalues = np.array(list(map(list,peigenvals[:,0])))
-        eigenvalues-= fermi_energy;#Compute the eigenvalues
-
-        #Determine the important kpoints
-        relevant_kpoints = np.any(np.abs(eigenvalues - fermi_energy) < tol, axis=1);
-        #Select the kpoints
-        kpoints = kpoints[relevant_kpoints];
-
-        if proj_op is None:
-            return kpoints,eigvals[relevant_kpoints];
-
-        #If one requires a projection, select the kpoints and then use them to compute the projections
-        
-        peigvals = np.array(list(map(list,peigenvals[:,1])))
-        return kpoints,peigvals[relevant_kpoints];
-
-    
-    def refine_kpoints(self, kpoints):
-
-        nkp= 2*len(kpoints);
-        kmin = np.array([ np.min(kpoints[:,0]),np.min(kpoints[:,1]),0 ] );
-        kmax = np.array([ np.max(kpoints[:,0]),np.max(kpoints[:,1]),0 ] );
-        kpoints= np.array(list(np.ndindex((nkp,nkp,1))))/(nkp-1)*(kmax-kmin) + kmin ;#kpoints in recpricola lattice vectors
-
-        return kpoints;
-    
-    
-    def compute_DOS(self, energies,gridpoints, fermi_energy = 0.0, broadening = 0.1,  kpoints = None ,proj_op = None ):
-        
-        if kpoints is None:
-            n1,n2,n3 = gridpoints
-            kpoints = ( np.mgrid[0:1:(n1+1)*1j, 0:1:(n2+1)*1j, 0:1:(n3+1)*1j].T)[:-1,:-1,:-1].reshape(n1*n2*n3,3);
-
-        eta=broadening;
-        def gaussian(x):
-            return np.exp( -(x/eta)**2/2 )/eta/np.sqrt(2*np.pi)
-
-        dim = len(kpoints);
-        peigenvals  = self.compute_dispersion(kpoints, proj_op=proj_op)
-        eigenvalues = np.array(list(map(list,peigenvals[:,0]))).flatten()
-        eigenvalues -= fermi_energy;#Compute the eigenvalues
-        
-        
-        if proj_op is None:
-            return np.array( [np.sum(gaussian(eigenvalues-EF)) for EF in energies] )/dim;
-
-        P = np.array(list(map(list,peigenvals[:,1]))).flatten()
-        return np.array( [np.sum(P*gaussian(eigenvalues-EF)) for EF in energies] )/dim;
-
-
-    def band_spin_texture(self, kpoints, bidx, uop = None, vop = None, zop=None, zlims = None, ax=None ):
-        #Compute first the mean values using the model
-
-        Ops= [uop, vop, zop ] ;
-
-        def get_band_data( x, bidx ):
-            y = None;
-            if x.shape[0] == kpoints.shape[0]:
-                y = x;
-            else:
-                y = self.compute_dispersion(kpoints, proj_op = x )[:,1];
-            y = np.array(list(map(list,y))).T;
-            return y[bidx];
-
-        #If is a compatible array of data, add it directly to Ops
-        Ops= [ get_band_data(op,bidx) for op in Ops ];
-
-        uop, vop, zop = Ops;
-        KX,KY,KZ = np.transpose(kpoints);
-
-        # Create triangulation.
-        triang = mtri.Triangulation(KX, KY)
-
-        def get_mask( x, y, z ):
-            return mtri.CubicTriInterpolator(triang, z, kind='geom')(x, y) > 0.;
-
-        def xypoints( npts ):
-            return np.meshgrid(np.linspace(KX.min(), KX.max(), npts), np.linspace(KY.min(), KY.max(), npts));
-
-        # Set up the figure
-        if( ax is None ):
-            fig, ax = plt.subplots(dpi=400);
-
-        # Interpolate to regularly-spaced quad grid.
-        xi, yi = xypoints(npts=100)
-        fzi    = mtri.CubicTriInterpolator(triang, zop, kind='geom');
-        zi     = np.ma.array( fzi(xi,yi) , mask=get_mask(xi, yi,zop) )
-
-        cs = ax.contourf(xi, yi, zi,levels=100, cmap="bone")
-#        fig.colorbar(cs, ax=ax, shrink=0.9 , label=r"$\rm Energy (meV)$")
-        # This is the fix for the white lines between contour levels
-        for c in cs.collections:
-            c.set_edgecolor("face")
-
-
-        xi, yi = xypoints(npts=20)
-        fui= mtri.CubicTriInterpolator(triang, uop, kind='geom');
-        fvi= mtri.CubicTriInterpolator(triang, vop, kind='geom');
-        ui = np.ma.array(fui(xi, yi), mask=get_mask(xi, yi,zop));
-        vi = np.ma.array(fvi(xi, yi), mask=get_mask(xi, yi,zop));
-        Q = ax.quiver(xi, yi, ui, vi, color="C1",  scale=15, width=0.022/4);
-
-        return ax;
-    
-    
-    def createRandomPhase(self, dims, output, mask=None):
-        def linearize( i , dims ):
-            x,y,z =i ;
-            nx,ny,nz =dims ;
-            return z*nx*ny + y*nx + x;
-
-        nx,ny,nz = dims;
-        numstates  = matdim = len(self.ham_operator([0,0,0]))
-        dim = np.prod(dims);
-        states = np.zeros((numstates,numstates*dim), dtype=complex)
-        grid =(np.mgrid[0:nx,0:ny,0:nz].T).reshape( dim ,3);
-
-        for kp in grid:
-            if mask is not None and mask(kp/dims):
-                continue;
-            else:
-                k = linearize(kp,dims);
-                kp = kp/dims;
-                w, vs  = np.linalg.eigh(self.ham_operator(kp))
-                vs = vs.T;
-                for i,v in enumerate(vs):
-                    states[i,k*matdim:(k+1)*matdim]= np.exp(2j*np.pi*np.random.random())*v;
-
-        for s in range(numstates):
-            for i in range(matdim):
-                S = states[s][i::matdim].reshape(dims).T
-                FS = np.fft.fftn(S)
-                states[s][i::matdim] = FS.T.flatten();
-            states[s] /=np.linalg.norm(states[s]);
-
-        f = open(output,'w')
-        f.write("vector\n")
-        f.write( str(numstates)+"\n")
-        f.write( str(dim*matdim)+"\n")
-
-        for state in states:
-            for x in state:
-                f.write(str(x.real)+" "+str(x.imag)+"\n")
+  
         f.close()
 
     def projected_bands(self, proj_ops  ):
