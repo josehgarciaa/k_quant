c006ee99e105e2ca2bb821e2e60b08af36f0bc22
Jose Hugo Garcia (local) <ljgarcia@1605M010.icn2.net>
2022-10-30 10:38:30 +0100

    Fixed documentation

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -29,6 +29,8 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
+html_theme = 'alabaster'
+
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
