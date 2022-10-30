979c7dc0ef1acacdfa5ef28875bffaa8874cdd1c
Jose Hugo Garcia (local) <ljgarcia@1605M010.icn2.net>
2022-10-30 12:08:41 +0100

    Fixed documentation

--- sphinx/modify_documentation.sh
+++ sphinx/modify_documentation.sh
@@ -2,8 +2,8 @@
 
 rm -fR _build/*
 rm -fR ../docs/*
-touch _build/html/.nojekyll
 sphinx-apidoc -f -o autodoc/ ../k_quant/
 make html
 cp -va _build/html/* ../docs/
+touch ../docs/.nojekyll
 git add ..
