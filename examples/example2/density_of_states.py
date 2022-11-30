d3272581f52c2a3f208f3a2a11ab8a776f57722d
josehgarciaa <josehugo.garcia@protonmail.com>
2022-11-30 13:43:02 +0100

    System class now computes the hamiltonian and the U matrices

--- examples/example2/density_of_states.py
+++ examples/example2/density_of_states.py
@@ -3,7 +3,20 @@ import numpy as np
 from system import System 
 
 
-wann_syst = System( dimensions = (100,1,1), w90_inp="linear_chain")
+wann_syst = System( dimensions = (3,3,1), w90_inp="PtSe2" )
 
-print(wann_syst.Hamiltonian() )
+print("Computing eigenvalues")
+Es = wann_syst.BlochEigenvalues();
 
+print("Computing hamiltonian in eigenvalue basis")
+MEs=  wann_syst.BlochToEigen( wann_syst.Hamiltonian() )
+
+print( np.sum([ np.sum( np.abs(E1 - np.diag(ME2)) ) for E1, ME2 in zip(Es, MEs )] ) ) 
+
+
+
+    def compute_moments(self,broadening):
+        return self;
+        
+    def spectral_average(self,energies):
+        return self;
--- k_quant/system.py
+++ k_quant/system.py
@@ -1,6 +1,10 @@
+import numpy as np
 from numpy import meshgrid, transpose,linspace, apply_along_axis
 from k_quant.models.wannier import WannierSystem  
-from .lattice import Lattice
+from k_quant.lattice import Lattice
+from k_quant.linalg.sparse import BlockDiag
+
+
 class System():
     """ Storage and handle the momentum-space model and structural information of the system  
 
@@ -20,48 +24,51 @@ class System():
     
     dimensions= None;
     lattice   = Lattice();
-    Ham_fun   = None;
-    reciprocal_lattice = None; 
+    ham_fun   = None;
+    rec_lat   = None; 
+    ham_op    = None;
+    Uop       = None;
+    basis     = "bloch"
 
     def __init__(self, dimensions, **kwargs):
 
-        self.dimensions= dimensions;
-        self.bz_points = self.bz_points();
+        self.dimensions= dimensions ;
+        self.rec_lat   = self.ReciprocalLattice();
 
         #When the user submit a Wannier90 input, use it to initialice the system
         if "w90_inp" in kwargs:
             w90_fname = kwargs["w90_inp"];
-            syst = WannierSystem(w90_fname);
+            syst = WannierSystem(label=w90_fname);
             self.lattice = Lattice( primitive_vectors = syst.primitive_vectors,
                                     orbital_positions = syst.primitive_vectors );
-            self.Ham_fun = syst.hamiltonian;
+            self.ham_fun = syst.Hamiltonian;
 
         if "model" in kwargs:
             w90_fname = kwargs["model"];
             syst = WannierSystem(w90_fname);
             self.lattice = Lattice( primitive_vectors = syst.primitive_vectors,
                                     orbital_positions = syst.primitive_vectors );
-            self.Ham_fun = syst.hamiltonian;
+            self.ham_fun = syst.hamiltonian;
 
 
             
-    def reciprocal_lattice(self):
+    def ReciprocalLattice(self):
         """Returns the points in the first Brilluoin zone that defines the reciprocal lattice.
             
             Note:
             This functions compute these points on a first call. Therefore, that first call could take significantly more time than subsequent calls.    
         
         """
-        if self.reciprocal_lattice is None:
+        if self.rec_lat is None:
             
             dim = self.dimensions;
             meshkgrid    = meshgrid( *[ linspace(0,1,d, endpoint=False)  for d in dim ], indexing='ij');  
-            self.reciprocal_lattice = transpose( [ x.flatten() for x in meshkgrid] );      
+            self.rec_lat = transpose( [ x.flatten() for x in meshkgrid] );      
 
-        return self.reciprocal_lattice;   
+        return self.rec_lat;   
             
 
-    def H_k(kpoint):
+    def H_k(self, kpoint):
         """ The k-dependent hamiltonian of the system evaluted in a particular kpoint
 
         :param kpoint: An arbitrary kpoint within the Brilluoin zone
@@ -69,16 +76,45 @@ class System():
         :return: A matrix of the hamiltonian evaluted at that particular kpoint
         :rtype: Array
         """
-        return self.Ham_fun(kpoint);
+        return self.ham_fun(kpoint);
 
+
+    def Umatrix(self):
+        """The hamiltonian matrix in the Block diagonal sparse format the first Brilluoin zone that defines the reciprocal lattice.
+
+        :return: A complex sparse matrix in the block-diagonal form
+        """
+        return apply_along_axis( self.U_k, arr=self.rec_lat, axis=1 );
+            
 
     def Hamiltonian(self):
         """The hamiltonian matrix in the Block diagonal sparse format the first Brilluoin zone that defines the reciprocal lattice.
 
         :return: A complex sparse matrix in the block-diagonal form
         """
-        return apply_along_axis( self.H_k, arr=self.reciprocal_lattice, axis=1 );
-            
+        if self.ham_op is None:
+            self.ham_op = apply_along_axis( self.H_k, arr=self.rec_lat, axis=1 );
+
+        return self.ham_op;
+
+    def Umatrix(self):
+        if self.Uop is None:
+            self.Uop = np.linalg.eigh( self.Hamiltonian() )[1];
+        
+        return self.Uop;
+
+
+    def BlochToEigen(self, blochOP):
+        UR = self.Umatrix();
+        UL = np.conj( np.transpose( UR, (0,2,1) ) );
+        op       = 'ijk,ikl->ijl';
+        axis_dot = np.einsum        
+        return axis_dot(op, UL, axis_dot(op, blochOP, UR ) );
+    
+    def BlochEigenvalues(self):
+        return np.linalg.eigvalsh( self.Hamiltonian() )
+        
+        
             
     
     
