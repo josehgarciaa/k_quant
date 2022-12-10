c8a0d2afd54fe2edbc713bc4dab64fcc9bf85dfa
josehgarciaa <josehugo.garcia@protonmail.com>
2022-12-10 16:32:47 +0100

    testing new documentation

--- docs/_sources/index.rst.txt
+++ docs/_sources/index.rst.txt
@@ -1,7 +1,3 @@
-.. k_quant documentation master file, created by
-   sphinx-quickstart on Sat Oct 29 14:03:16 2022.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
 
 What is :math:`{\boldsymbol k}` Quant?
 ======================================
@@ -12,6 +8,10 @@ in long journey towards generating quantitative predictions of measurable quanti
 a high-level Python package which aims to provide an many different functions to compute
 spectral properties from the Hamiltonian such as bandstructures and their spin and orbital projections, 
 conductivities, spin Hall conductivities, and density of states. 
+
+
+.. automodule:: k_quant.kpm
+    :members:
 
 .. toctree::
    :titlesonly:
--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -1,55 +1,42 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../')) 
+cwd = os.getcwd()
 
 
 # -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'k_quant'
+
+project = 'k Quant'
 copyright = '2022, Jose H. Garcia'
 author = 'Jose H. Garcia'
-
-# The full version, including alpha/beta/rc tags
 release = '0.0.0.0'
 
-
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = [
-]
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.mathjax', 'sphinx.ext.napoleon']
+templates_path = ['_templates'] #path relative to the config.py file
+#exclude_patterns = ['*_build', '*_static', '.DS_Store', '.git*', '*egg-info*', '*pycache*' ]
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+html_theme = 'alabaster'
 
 
 # -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'alabaster'
+html_static_path = ['_static'] #path relative to the config.py file
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_theme = 'kotti_docs_theme'
+#html_theme_path = [kotti_docs_theme.get_theme_dir()]
+
+
+#Module options
+#add_module_names = False
