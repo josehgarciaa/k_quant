53252007e966fbfbdb2b068841e45ddbef5330c3
josehgarciaa <josehugo.garcia@protonmail.com>
2022-12-10 14:50:35 +0100

    modified documentation

--- docs/_sources/index.rst.txt
+++ docs/_sources/index.rst.txt
@@ -1,20 +1,21 @@
 .. k_quant documentation master file, created by
-   sphinx-quickstart on Sat Dec 10 14:44:43 2022.
+   sphinx-quickstart on Sat Oct 29 14:03:16 2022.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to k_quant's documentation!
-===================================
+What is :math:`{\boldsymbol k}` Quant?
+======================================
+
+There are multiple situations where researchers create Hamiltonian models in the momentum space 
+by exploiting the Bloch's theorem, symmetries, and intuition. However, a Hamiltonian is just the first step
+in long journey towards generating quantitative predictions of measurable quantities. ``k-quant`` is
+a high-level Python package which aims to provide an many different functions to compute
+spectral properties from the Hamiltonian such as bandstructures and their spin and orbital projections, 
+conductivities, spin Hall conductivities, and density of states. 
 
 .. toctree::
-   :maxdepth: 2
-   :caption: Contents:
+   :titlesonly:
 
-
-
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
+#   Examples <example>
+#   Modules <modules>
+   
---
+++ sphinx/conf.py
@@ -0,0 +1,42 @@
+ç# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../k_quant/'))
+cwd = os.getcwd()
+print("current path",cwd)
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+
+project = 'k Quant'
+copyright = '2022, Jose H. Garcia'
+author = 'Jose H. Garcia'
+release = '0.0.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.mathjax']
+templates_path = ['build/_templates']
+#exclude_patterns = ['*_build', '*_static', '.DS_Store', '.git*', '*egg-info*', '*pycache*' ]
+
+html_theme = 'alabaster'
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_static_path = ['build/_static']
+
+#html_theme = 'kotti_docs_theme'
+#html_theme_path = [kotti_docs_theme.get_theme_dir()]
+
+
+#Module options
+#add_module_names = False
