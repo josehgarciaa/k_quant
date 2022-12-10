ea164ab93ed95e11e55aa915cad1bfa6ab97fd55
josehgarciaa <josehugo.garcia@protonmail.com>
2022-12-10 16:40:04 +0100

    included information of the kpm module

--- k_quant/kpm.py
+++ k_quant/kpm.py
@@ -24,14 +24,59 @@ import time
 
 
 class Density:
-    """ A spectral density, is a quantity defined as <X> = Tr[ X delta(H-E) ]. From its definition, 
+    """The summary line for a class docstring should fit on one line.
+
+        If the class has public attributes, they may be documented here
+        in an ``Attributes`` section and follow the same formatting as a
+        function's ``Args`` section. Alternatively, attributes may be documented
+        inline with the attribute's declaration (see __init__ method below).
+
+        Properties created with the ``@property`` decorator should be documented
+        in the property's getter method.
+
+        Attributes
+        ----------
+        attr1 : str
+            Description of `attr1`.
+        attr2 : :obj:`int`, optional
+            Description of `attr2`.
+
+    
+        A spectral density, is a quantity defined as <X> = Tr[ X delta(H-E) ]. From its definition, 
         this quantity computes how much of the operator X will be measured at a given en energy. 
         
         In the kpm module, the delta function is computed by expanding it in terms of Chebyshev polynomials
         and regularized using a kernel function.  
 
         
+        Exceptions are documented in the same way as classes.
+
+        The __init__ method may be documented in either the class level
+        docstring, or as a docstring on the __init__ method itself.
+
+        Either form is acceptable, but the two should not be mixed. Choose one
+        convention to document the __init__ method and be consistent with it.
+
+        Note
+        ----
+        Do not include the `self` parameter in the ``Parameters`` section.
+
+        Parameters
+        ----------
+        msg : str
+            Human readable string describing the exception.
+        code : :obj:`int`, optional
+            Numeric error code.
+
+        Attributes
+        ----------
+        msg : str
+            Human readable string describing the exception.
+        code : int
+            Numeric error code.
+
     """
+    
     
     moments = None;
     Ham     = None;
@@ -43,7 +88,29 @@ class Density:
     num_orbs= None;
     
     def __init__(self,system,  kernel = "jackson",  Op=None, bounds=None):
-        """Construct an instance of the Density class 
+        """Example of docstring on the __init__ method.
+
+        The __init__ method may be documented in either the class level
+        docstring, or as a docstring on the __init__ method itself.
+
+        Either form is acceptable, but the two should not be mixed. Choose one
+        convention to document the __init__ method and be consistent with it.
+
+        Note
+        ----
+        Do not include the `self` parameter in the ``Parameters`` section.
+
+        Parameters
+        ----------
+        param1 : str
+            Description of `param1`.
+        param2 : :obj:`list` of :obj:`str`
+            Description of `param2`. Multiple
+            lines are supported.
+        param3 : :obj:`int`, optional
+            Description of `param3`.
+
+        Construct an instance of the Density class 
 
         Args:
             system (object): A system as defined in the system module. 
