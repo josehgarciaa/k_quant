dabeb5a518d591ff0170a8f0b381ffec71e177e3
Jose Hugo García <josehugo.garcia@icn2.cat>
2024-06-05 11:34:16 +0200

    position operator implemented

--- cppcore/include/wannier90/w90_operators.hpp
+++ cppcore/include/wannier90/w90_operators.hpp
@@ -18,6 +18,11 @@ namespace W90{
         //Getters
         int GetNumGridPoints() const { 
             return num_grid_points; }
+
+
+        
+
+
     };
 
     class Hamiltonian : public Operator {
